bf0a17d15a5c97af1e61f82ebb778d5e262de99e
zhangxiaojun <zhangxiaojun@aihuaiedu.cn>
2019-08-27 14:44:11 +0800

    格式化代码

--- src/router.ts
+++ src/router.ts
@@ -1,33 +1,33 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
-const Home = () => import('@/views/Home.vue')
-const TestCenter = () => import('@/views/testCenter/Index.vue')
-Vue.use(Router)
+const Home = () => import("@/views/Home.vue");
+const TestCenter = () => import("@/views/testCenter/Index.vue");
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ '@/views/About.vue')
+        import(/* webpackChunkName: "about" */ "@/views/About.vue")
     },
     {
-      path: '/testCenter',
-      name: 'TestCenter',
+      path: "/testCenter",
+      name: "TestCenter",
       component: TestCenter,
       meta: {
-        title: 'typescript base项目'
+        title: "typescript base项目"
       }
     }
   ]
-})
+});
