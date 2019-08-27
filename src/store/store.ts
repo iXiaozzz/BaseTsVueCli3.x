fbbff900817e97fd767b513ff495dc6917666e0e
zhangxiaojun <zhangxiaojun@aihuaiedu.cn>
2019-08-27 17:00:17 +0800

    使用vuex-class组件使用vuex

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,42 @@
 import Vue from "vue";
-import Vuex from "vuex";
+import Vuex, { GetterTree, ActionTree, MutationTree, Commit } from "vuex";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
-    userName: ""
+interface State {
+  username: string;
+  count: number;
+}
+const state: State = {
+  username: "iXiao",
+  count: 0
+};
+const getters: GetterTree<State, any> = {
+  username: (state: State) => state.username,
+  count: (state: State) => state.count
+};
+
+const mutations: MutationTree<State> = {
+  UPDATE_USERNAME(state: State, username: string) {
+    state.username = username;
   },
-  mutations: {},
-  actions: {}
+  UPDATE_COUNT(state: State, count: number) {
+    state.count = count;
+  }
+};
+
+const actions: ActionTree<State, any> = {
+  updateUserName(context: { commit: Commit }, username: string) {
+    context.commit("UPDATE_USERNAME", username);
+  },
+  updateCount(context: { commit: Commit }, count: number) {
+    context.commit("UPDATE_COUNT", count);
+  }
+};
+
+export default new Vuex.Store({
+  state,
+  getters,
+  mutations,
+  actions
 });
